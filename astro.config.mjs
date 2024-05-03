181e94450ab2fdd9f6fd585a7d9a422f87e4095c
Lizeth castillo <64451121+lizethcas@users.noreply.github.com>
2024-05-03 05:56:19 -0500

    update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import analogjsangular from "@analogjs/astro-angular";
 
 import vue from "@astrojs/vue";
 
@@ -9,5 +8,5 @@ export default defineConfig({
   site: 'https://lizethcas.github.io',
   integrations: [
     
-    tailwind(), analogjsangular(), vue()]
+    tailwind(), vue()]
 });
--- src/components/FaqComponent.vue
+++ src/components/FaqComponent.vue
@@ -25,7 +25,7 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, ref } from 'vue'
+import { ref } from 'vue'
 
 const props = defineProps({
     title: String,
